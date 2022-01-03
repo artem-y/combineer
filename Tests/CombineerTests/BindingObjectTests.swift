46406ce53a6c22bd6b154a6c29135f0b49b92255
Artem Yelizarov <artem.y.inbox@gmail.com>
2022-01-03 19:24:28 +0200

    Add tests for binding methods.

--- Tests/CombineerTests/BindingObjectTests.swift
+++ Tests/CombineerTests/BindingObjectTests.swift
@@ -3,17 +3,82 @@ import XCTest
 @testable import Combineer
 
 final class BindingObjectTests: XCTestCase {
-    typealias SUT = BindingObject
+    private var sut: MockBindingObject!
+    private var stringPulisher: Publishers.Sequence<String, Never>!
+    private var stringSubject: PassthroughSubject<String, Never>!
+
+    // MARK: - Lifecycle
+
+    override func setUp() {
+        sut = MockBindingObject()
+        stringPulisher = "".publisher
+        stringSubject = PassthroughSubject<String, Never>()
+    }
+
+    override func tearDown() {
+        sut = nil
+        stringPulisher = nil
+        stringSubject = nil
+    }
+
+    // MARK: - Test bind
 
     func test_bind_storesSubscriptionInCancellables() {
-        let object = MockBindingObject()
-        object.bind("".publisher, valueHandler: { _ in })
-        XCTAssertEqual(object.cancellables.count, 1)
+        sut.bind(stringPulisher, valueHandler: { _ in })
+        XCTAssertEqual(sut.cancellables.count, 1)
+    }
+
+    func test_bind_assignsCorrectReceiveHandler() {
+        let passedValue = "passed value"
+        let expectation = makeReceivedValueExpectation()
+
+        sut.bind(stringSubject) { value in
+            XCTAssertEqual(value, passedValue)
+            expectation.fulfill()
+        }
+
+        DispatchQueue.main.async { [weak self] in
+            self?.stringSubject.send(passedValue)
+        }
+
+        wait(for: [expectation], timeout: 1)
+    }
+
+    // MARK: - Test bindOnMainQueue
+
+    func test_bindOnMainQueue_storesSubscriptionInCancellables() {
+        sut.bindOnMainQueue(stringPulisher, valueHandler: { _ in })
+        XCTAssertEqual(sut.cancellables.count, 1)
+    }
+
+    func test_bindOnMainQueue_whenValueSentFromOtherQueue_receivesOnMainQueue() {
+        let expectation = makeReceivedValueExpectation()
+
+        sut.bindOnMainQueue(stringSubject) { _ in
+            XCTAssert(Thread.current.isMainThread, "Not on main thread.")
+            expectation.fulfill()
+        }
+
+        DispatchQueue.global().async { [weak self] in
+            self?.stringSubject.send("123")
+        }
+
+        wait(for: [expectation], timeout: 1)
     }
 }
 
+// MARK: - Private Methods
+
 extension BindingObjectTests {
-    final class MockBindingObject: BindingObject {
+    private func makeReceivedValueExpectation() -> XCTestExpectation {
+        expectation(description: "Receive value from publisher")
+    }
+}
+
+// MARK: - Helpers
+
+extension BindingObjectTests {
+    private final class MockBindingObject: BindingObject {
         var cancellables: Set<AnyCancellable> = []
     }
 }
